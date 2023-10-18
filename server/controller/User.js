8bc7b48005da6cd9ce3e9b99f1d74d16fa920d71
BinLama <binlama@yahoo.com>
2023-10-18 11:49:59 -0700

    add: delete and update user

--- server/controller/User.js
+++ server/controller/User.js
@@ -6,92 +6,147 @@ const getAllUser = async (req, res) => {
     const users = await User.findAll({
       attributes: { exclude: ["hash_pw"] },
     });
-    if (!users) throw new Error(`User with id: ${id} not found.`);
-    res.json(users);
+    return res.status(200).json({ users: users });
   } catch (error) {
-    console.log(error);
-    res.status(500).send("Server nto reached");
+    res.status(500).json({ msg: error });
   }
 };
 
 const getUser = async (req, res) => {
   try {
     const { id } = req.params;
+    console.log(id);
     const user = await User.findByPk(id, {
       attributes: { exclude: ["hash_pw"] },
     });
-    if (!user) throw new Error(`User with id: ${id} not found.`);
-    res.json(user);
+
+    if (!user) {
+      return res.status(404).json({ msg: `No user with id: ${id}` });
+    }
+
+    res.status(200).json({ user: user });
   } catch (error) {
-    console.log(error);
+    return res.status(500).json({ msg: error });
   }
 };
 
 const createUser = async (req, res) => {
-  console.log(req.body);
-  const { f_name, l_name, username, email, password } = req.body;
+  try {
+    const { f_name, l_name, username, email, password } = req.body;
 
-  if (!(f_name && l_name && email && password && username)) {
-    res.stauts(400).json("Not all information was provided");
-    return;
-  }
-
-  if (!validateEmail(email)) {
-    res.status(400).json(`Please enter a proper email.`);
-    return;
-  }
-
-  // check if email exits on the server
-  const emailInDB = await User.findOne({
-    where: {
-      email: email,
-    },
-    attributes: ["email", "username"],
-  });
-
-  console.log(emailInDB);
-  if (emailInDB) {
-    res.status(409).json(`User with ${email} already exists.`);
-    return;
-  }
-
-  const user = await User.create(
-    {
-      username: username,
-      f_name: f_name,
-      l_name: l_name,
-      email: email,
-      hash_pw: password,
-      setting: {},
-      color: {},
-    },
-    {
-      include: [User.setting, User.color],
+    if (!(f_name && l_name && email && password && username)) {
+      return res.stauts(400).json({ msg: "Not all information was provided" });
     }
-  );
 
-  res.status(200).json({ user });
+    if (!validateEmail(email)) {
+      return res.status(400).json({ msg: `Please enter a proper email.` });
+    }
+
+    // check if email exits on the server
+    const emailInDB = await User.findOne({
+      where: {
+        email: email,
+      },
+      attributes: ["email", "username"],
+    });
+
+    // check if username exits on the server
+    const usernameInDB = await User.findOne({
+      where: {
+        username: username,
+      },
+      attributes: ["email", "username"],
+    });
+
+    if (emailInDB || usernameInDB) {
+      return res.status(409).json({ msg: `User already exists.` });
+    }
+
+    const user = await User.create(
+      {
+        username: username,
+        f_name: f_name,
+        l_name: l_name,
+        email: email,
+        hash_pw: password,
+        setting: {},
+        color: {},
+      },
+      {
+        include: [User.setting, User.color],
+      }
+    );
+
+    const newUser = Object.fromEntries(
+      Object.entries(user.dataValues).filter(([key, val]) => {
+        if (key != "hash_pw") {
+          return [key, val];
+        }
+      })
+    );
+
+    res.status(200).json({ user: newUser });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
   // add settings, color,
   // const user = User.build()
 };
 
-const updateUser = async (req, res) => {
-  console.log();
-  // try {
-  //   const { id } = req.params;
-  //   const user = await User.update(id, {
-  //     attributes: ["f_name", "l_name", "id", "email", "username"],
-  //   });
-  //   if (!user) throw new Error(`User with id: ${id} not found.`);
-  //   res.json(user);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  res.send(`Editing User: ${req.params.id}`);
+const deleteUser = async (req, res) => {
+  // have to be very careful with deleting a user.
+  // first need to validate of it's the proper user
+  // then I will have to give me some days to delete the user.
+  // if they log back in, then don't delete the user anymore
+  //
+  try {
+    const { id } = req.params;
+
+    const deleted = await User.destroy({
+      where: {
+        id: id,
+      },
+      force: true,
+    });
+
+    if (deleted === 0) {
+      return res.status(404).json({ msg: `User with id: ${id} not found.` });
+    }
+
+    res.status(200).json({
+      msg: `Deleting User: ${id}`,
+      user: deleted,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteUser = (req, res) => {
-  res.send(`Deleting User: ${req.params.id}`);
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { f_name, l_name, username } = req.body;
+
+    if (f_name === "" || l_name === "" || username === "") {
+      return res.status(400).json({ msg: `Please insert proper information` });
+    }
+
+    const user = await User.update(req.body, { where: { id: id } });
+
+    if (user[0] === 1) {
+      const newUser = await User.findByPk(id, {
+        attributes: { exclude: ["hash_pw"] },
+      });
+
+      return res.status(200).json({ user: newUser });
+    }
+
+    res
+      .status(404)
+      .json({ msg: `No user with id: ${id} or no new information to update` });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
--- server/router/user.js
+++ server/router/user.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const { getAllUser, getUser, createUser } = require("../controller/User");
+const {
+  getAllUser,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require("../controller/User");
 const { getUserSetting } = require("../controller/Setting");
 
 const userRouter = express.Router();
 
 // getting user info
-userRouter.get("/", getAllUser).post("/", createUser);
+userRouter.route("/").get(getAllUser).post(createUser);
 
 // getting single user info
-userRouter.get("/:id", getUser);
+userRouter.route("/:id").get(getUser).delete(deleteUser).patch(updateUser);
 
 // getting settings
 userRouter.get("/:id/setting", getUserSetting);
+
 module.exports = { userRouter };
