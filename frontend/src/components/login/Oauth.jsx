d1f4b845e7f6befa2cecbf3910d1aedb093dc89c
Bin Lama <binlama@yahoo.com>
2024-04-02 15:26:24 -0700

    add navigation to home on continue as guest

--- frontend/src/components/login/Oauth.jsx
+++ frontend/src/components/login/Oauth.jsx
@@ -1,13 +1,22 @@
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
 
 const Oauth = () => {
+  const navigate = useNavigate();
+
+  const continueAsGuestClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="oauth">
       <button className="btn oauth__google">
         <FcGoogle />
         <p>Continue with Google</p>
       </button>
-      <button className="btn oauth__guest">Continue as Guest</button>
+      <button className="btn oauth__guest" onClick={continueAsGuestClick}>
+        Continue as Guest
+      </button>
     </div>
   );
 };
